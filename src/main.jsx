301094394cc837f4ae641b86ff1f1f49bee9d971
Vivek Neer <139262048+VivekNeer@users.noreply.github.com>
2024-09-20 23:52:07 +0530

    Navbar

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import { CssBaseline, ThemeProvider } from '@mui/material';  // Update to use Material UI ThemeProvider
+import theme from './theme';  // Import the theme from theme.js
 import './index.css';  // Ensure this path is correct
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ThemeProvider theme={theme}> {/* Use ThemeProvider from @mui/material */}
+      <BrowserRouter>
+        <CssBaseline />
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
   </React.StrictMode>
 );
